58667a96f16a51e197d0faf48935044c8fea753c
Sudheer Boppana <104917342+sudheer-2001@users.noreply.github.com>
2024-01-10 00:16:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -72,13 +72,13 @@ $(document).ready(function(){
     });
 });
 
-var form = document.getElementById("sheetdb-form")
-form.addEventListener('submit',function(e){
-    e.preventDefault()
-    fetch(form.action,{
-        method : "POST",
-        body : new Formdata(document.getElementById("sheetdb-form"))
+var form = document.getElementById("sheetdb-form");
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    fetch(form.action, {
+        method: "POST",
+        body: new FormData(document.getElementById("sheetdb-form"))
     }).then(
-        response=>response.json()
-    )
-})
+        response => response.json()
+    );
+});
