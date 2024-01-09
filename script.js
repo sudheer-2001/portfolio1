21b0af135fd533aa872cbda9050071883807136b
Sudheer Boppana <104917342+sudheer-2001@users.noreply.github.com>
2024-01-09 23:34:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,3 +71,14 @@ $(document).ready(function(){
         }
     });
 });
+
+var form = document.getElementById("sheetdb-form")
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    fetch(form.action,{
+        method : "POST",
+        body : new Formdata(document.getElementById("sheetdb-form"))
+    }).then(
+        response=>response.json()
+    )
+})
